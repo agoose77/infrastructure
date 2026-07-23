683c487cae2f4fc426f5406b2dab6665183f2e85
Angus Hollands <goosey15@gmail.com>
2026-07-23 11:31:01 +0100

    wip: move deployer

---
+++ packages/deployer/dev_commands/generate/hub_asset/cluster_entry.py
@@ -0,0 +1,28 @@
+import jinja2
+import typer
+from deployer.utils.file_acquisition import REPO_ROOT_PATH
+
+from .hub_asset_app import hub_asset_app
+
+
+@hub_asset_app.command()
+def cluster_entry(
+    cluster_name: str = typer.Option(
+        ..., prompt="The name of the cluster where the hub will live"
+    ),
+    hub_name: str = typer.Option(..., prompt="The name of the hub"),
+):
+    """
+    Outputs the relevant cluster.yaml hub entry that the engineer can then manually copy-paste into the relevant cluster.yaml file.
+    """
+
+    vars = {
+        "cluster_name": cluster_name,
+        "hub_name": hub_name,
+    }
+
+    with open(
+        REPO_ROOT_PATH / "config/clusters/templates/common/cluster-entry.yaml"
+    ) as f:
+        cluster_hub_entry_template = jinja2.Template(f.read())
+    print(cluster_hub_entry_template.render(**vars))
